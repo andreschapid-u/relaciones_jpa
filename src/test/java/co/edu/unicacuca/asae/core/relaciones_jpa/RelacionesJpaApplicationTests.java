989c3ea8d36607d9ebb575bcd2895c5d7eb544b6
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2020-10-16 16:19:32 -0500

    first commit

---
+++ src/test/java/co/edu/unicacuca/asae/core/relaciones_jpa/RelacionesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicacuca.asae.core.relaciones_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
